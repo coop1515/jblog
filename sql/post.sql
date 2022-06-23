3a4d0a65062eb64af8fc1007050b1db4ba1357ef
김현석 <gustjr324@gmail.com>
2022-06-23 15:39:23 +0900

    ..

--- sql/post.sql
+++ sql/post.sql
@@ -2,8 +2,14 @@
 
 select * from post;
 
+select count(*) from post;
+
+select * from post group by category_no;
+
 insert into post values (null, '하이', 'ㅋㅋ', 16);
 
-select * from post order by no desc limit 1;
+select * from post where category_no = 3 order by no desc limit 1;
 
-select * from post a, category b where a.category_no = b.no and b.no = 4;
+select * from post a, category b where a.category_no = b.no and b.no = 10;
+
+select  distinct name, (select count(*) from post where category_no = 5) from category;
